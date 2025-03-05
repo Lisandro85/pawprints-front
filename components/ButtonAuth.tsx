37494d4cea4d0422735dbce456257ce7d4a0c0a1
Lisandro85 <lisandrobedotti@hotmail.com>
2025-03-05 09:54:53 +0100

    SessionAuthProvider

---
+++ components/ButtonAuth.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function ButtonAuth() {
+  const { data: session, status } = useSession();
+
+  console.log({ session, status });
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.userName} <br />
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded"
+          onClick={() => signOut()}
+        >
+          Sign out
+        </button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+        onClick={() => signIn()}
+      >
+        Sign in
+      </button>
+    </>
+  );
+}
